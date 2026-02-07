ee8060c46460fc1ee50b402d1fc77c3f39641013
EDUARDO PEREIRA NASCIMENTO <eduardo.pnascimento2@senacsp.edu.br>
2026-02-06 21:20:10 -0300

    test: criado testes unitarios

--- src/test/java/com/aula/calculadora/CalculadoraTest.java
+++ src/test/java/com/aula/calculadora/CalculadoraTest.java
@@ -1,5 +1,46 @@
 package com.aula.calculadora;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CalculadoraTest {
-    
+     //Arrange
+    Calculadora calculadoraTestes = new Calculadora();
+
+    @Test
+    void deveTestarSoma(){
+
+        //Act
+        int resultado = calculadoraTestes.somar(5, 5);
+        
+        //Assert
+        assertEquals(10,resultado);     
+        assertEquals(17, calculadoraTestes.somar(12, 5));   
+    }
+
+    @Test
+    void deveTestarDivisao(){
+
+        //Act
+        int resultado = calculadoraTestes.dividir(-5000, -10);
+        //Assert
+        assertEquals(500, resultado);
+
+    }
+
+    @Test
+    void deveSubtrairDoisNumeros(){
+
+        int resultado = calculadoraTestes.subtrair(20, 60);
+        assertEquals(-40,resultado);
+
+    }
+
+    @Test
+    void deveMultiplicarDoisNumeros(){
+
+        int resultado = calculadoraTestes.multiplicar(8, 7);
+        assertEquals(56, resultado);
+    }
 }
